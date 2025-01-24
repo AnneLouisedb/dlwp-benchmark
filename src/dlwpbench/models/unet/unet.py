0c6d4a3e44002398c312182b18d096fbf405e941
AnneLouisedb <annelouisedeboer@hotmail.com>
2025-01-24 12:33:24 +0100

    modern unet implementation

--- src/dlwpbench/models/unet/unet.py
+++ src/dlwpbench/models/unet/unet.py
@@ -7,6 +7,136 @@ from utils import CylinderPad
 from utils import HEALPixLayer
 import wandb
 
+# internal imports
+from blocks import MiddleBlock, ResiduaBlock
+
+
+class ModernUNet(th.nn.Module):
+    """
+    A ModernUNet implementation as by the PDE Refiner Paper
+    """
+
+    def __init__(
+        self, 
+        constant_channels: int = 4,
+        prescribed_channels: int = 0,
+        prognostic_channels: int = 1,
+        hidden_channels: list = [8, 16, 32],
+        n_convolutions: int = 2,
+        activation: th.nn.Module = th.nn.ReLU(),
+        context_size: int = 1,
+        mesh: str = "equirectangular",
+        norm: str = None,
+        middle_block: str = None, # configs
+        attention: bool = False,
+        **kwargs
+    ):
+        super(UNet, self).__init__()
+        if isinstance(activation, str): activation = eval(activation)
+
+        self.context_size = context_size
+        self.mesh = mesh
+        self.norm = norm
+        self.middle_block = middle_block
+        
+        in_channels = constant_channels + (prescribed_channels+prognostic_channels)*context_size
+        print("In channels UNet from initialization", in_channels)
+        self.encoder = ModernUNetEncoder(
+            in_channels=in_channels,
+            hidden_channels=hidden_channels,
+            n_convolutions=n_convolutions,
+            activation=activation,
+            attention = attention,
+            mesh=mesh
+        )
+        self.middle = MiddleBlock(
+            in_channels=hidden_channels[-1], 
+            #attention = attention,
+            norm = norm,
+            activation=activation,
+            mesh=mesh)
+      
+
+        self.decoder = ModernUNetDecoder(
+            hidden_channels=hidden_channels,
+            out_channels=prognostic_channels,
+            n_convolutions=n_convolutions,
+            activation=activation,
+            mesh=mesh
+        )
+
+    def _prepare_inputs(
+        self,
+        constants: th.Tensor = None,
+        prescribed: th.Tensor = None,
+        prognostic: th.Tensor = None
+    ) -> th.Tensor:
+        """
+        return: Tensor of shape [B, (T*C), H, W] containing constants, prescribed, and prognostic/output variables
+        """
+        tensors = []
+        if constants is not None: tensors.append(constants[:, 0])
+        if prescribed is not None: tensors.append(einops.rearrange(prescribed, "b t c h w -> b (t c) h w"))
+        if prognostic is not None: tensors.append(einops.rearrange(prognostic, "b t c h w -> b (t c) h w"))
+        return th.cat(tensors, dim=1)
+
+    def forward(
+        self,
+        constants: th.Tensor = None,
+        prescribed: th.Tensor = None,
+        prognostic: th.Tensor = None
+    ) -> th.Tensor:
+        """
+        ...
+        """
+        # Shapes of inputs, where (F) is the optional face dimension when using HEALPix data
+        # constants: [B, 1, C, (F), H, W]
+        # prescribed: [B, T, C, (F), H, W]
+        # prognostic: [B, T, C, (F), H, W]
+        if self.mesh == "healpix": B, _, _, F, _, _ = prognostic.shape
+        outs = []
+        for t in range(self.context_size, prognostic.shape[1]):
+            # For each t I want to store the loss in an array, and see how the prediction skill decreases over time
+          
+            t_start = max(0, t-(self.context_size))
+            if t == self.context_size:
+                # Initial condition
+                prognostic_t = prognostic[:, t_start:t]
+                x_t = self._prepare_inputs(
+                    constants=constants,
+                    prescribed=prescribed[:, t_start:t] if prescribed is not None else None,
+                    prognostic=prognostic_t # to forecast
+                )
+            else:
+                
+                # In case of context_size > 1, blend prognostic input with outputs from previous time steps
+                prognostic_t = th.cat(
+                    tensors=[prognostic[:, t_start:self.context_size],        # Prognostic input before context_size
+                             th.stack(outs, dim=1)[:, -self.context_size:]],  # Outputs since context_size
+                    dim=1
+                )
+                x_t = self._prepare_inputs(
+                    constants=constants,
+                    prescribed=prescribed[:, t-self.context_size:t] if prescribed is not None else None,
+                    prognostic=prognostic_t
+                )
+
+            enc = self.encoder(x_t)
+
+            enc2 = self.middle(enc)
+
+            out = self.decoder(x=enc2[-1], skips=enc2[::-1]) 
+            if self.mesh == "healpix": out = einops.rearrange(out, "(b f) tc h w -> b tc f h w", b=B, f=F)
+            out = prognostic_t[:, -1] + out
+
+            
+
+            outs.append(out)
+
+       
+        return th.stack(outs, dim=1)
+
+
 class UNet(th.nn.Module):
     """
     A UNet implementation.
@@ -22,6 +152,7 @@ class UNet(th.nn.Module):
         activation: th.nn.Module = th.nn.ReLU(),
         context_size: int = 1,
         mesh: str = "equirectangular",
+        
         **kwargs
     ):
         super(UNet, self).__init__()
@@ -29,6 +160,7 @@ class UNet(th.nn.Module):
 
         self.context_size = context_size
         self.mesh = mesh
+        
         in_channels = constant_channels + (prescribed_channels+prognostic_channels)*context_size
         print("In channels UNet from initialization", in_channels)
         self.encoder = UNetEncoder(
@@ -38,6 +170,7 @@ class UNet(th.nn.Module):
             activation=activation,
             mesh=mesh
         )
+       
         self.decoder = UNetDecoder(
             hidden_channels=hidden_channels,
             out_channels=prognostic_channels,
@@ -45,6 +178,7 @@ class UNet(th.nn.Module):
             activation=activation,
             mesh=mesh
         )
+
     
     def _prepare_inputs(
         self,
@@ -103,7 +237,7 @@ class UNet(th.nn.Module):
                 )
 
             enc = self.encoder(x_t)
-            
+
             out = self.decoder(x=enc[-1], skips=enc[::-1]) 
             if self.mesh == "healpix": out = einops.rearrange(out, "(b f) tc h w -> b tc f h w", b=B, f=F)
             out = prognostic_t[:, -1] + out
@@ -285,6 +419,192 @@ class UNetDecoder(th.nn.Module):
             x = th.cat([skips[l_idx], x], dim=1) if l_idx > 0 else x
             x = layer(x)
         return self.output_layer(x)
+    
+
+
+class ModernUNetEncoder(th.nn.Module):
+    """Unet encoder as used in the pde-refiner paper.
+    - Each downblock combines the ResiduaBlock and the AttentionBlock??"""
+
+    def __init__(
+        self,
+        in_channels: int = 2,
+        hidden_channels: list = [64, 128, 256, 1024],
+        n_convolutions: int = 2,
+        activation: th.nn.Module = th.nn.GELU(),
+        attention: bool = False,
+        mesh: str = "equirectangular"
+    ):
+        super(UNetEncoder, self).__init__()
+        self.layers = []
+        self.attn = th.nn.Identity() # attention is not yet implemented
+
+        channels = [in_channels] + hidden_channels
+
+        for c_idx in range(len(channels[:-1])):
+            layer = []
+            c_in = channels[c_idx]
+            c_out = channels[c_idx+1]
+
+            # Apply downsampling prior to convolutions if not in top-most layer
+            if c_idx > 0: layer.append(th.nn.AvgPool2d(kernel_size=2, stride=2, padding=0))
+
+            # Perform n convolutions (only half as many in bottom-most layer, since other half is done in decoder)
+            n_convs = n_convolutions//2 if c_idx == len(hidden_channels)-1 else n_convolutions
+            for n_conv in range(n_convs):
+                if mesh == "equirectangular":
+                    layer.append(CylinderPad(padding=1))
+                    # (1) norm (2) activation (3) convolution
+                    layer.append(ResiduaBlock(
+                        in_channels=c_in if n_conv == 0 else c_out,
+                        out_channels=c_out))
+                    # (4) Attention
+                    layer.append(self.attn)
+                    
+                             
+                # elif mesh == "healpix":
+                #     layer.append(HEALPixLayer(
+                #         layer=ResiduaBlock,
+                #         in_channels=c_in if n_conv == 0 else c_out,
+                #         out_channels=c_out))
+                #     layer.append(self.attn)
+                
+              
+            self.layers.append(th.nn.Sequential(*layer))
+
+        self.layers = th.nn.ModuleList(self.layers)
+
+    def forward(self, x: th.Tensor) -> list:
+        # Store intermediate model outputs (per layer) for skip connections
+        outs = []
+        for layer in self.layers:
+            x = layer(x)
+            outs.append(x)
+        return outs
+    
+class ModernUNetDecoder(th.nn.Module):
+
+    def __init__(
+        self,
+        hidden_channels: list = [64, 128, 256, 1024],
+        out_channels: int = 2,
+        n_convolutions: int = 2,
+        activation: th.nn.Module = th.nn.GELU(),
+        attention: bool = False,
+        mesh: str = "equirectangular"
+    ):
+        super(UNetDecoder, self).__init__()
+        self.layers = []
+        hidden_channels = hidden_channels[::-1]  # Invert as we go up in decoder, i.e., from bottom to top layers
+        self.attn = th.nn.Identity() # attention is not yet implemented
+        self.activation = activation
+        
+
+        for c_idx in range(len(hidden_channels)):
+            layer = []
+            c_in = hidden_channels[c_idx]
+            c_out = hidden_channels[c_idx]
+
+            # Perform n convolutions (only half as many in bottom-most layer, since other half is done in encoder)
+            n_convs = n_convolutions//2 if c_idx == 0 else n_convolutions
+            for n_conv in range(n_convs):
+                c_in_ = c_in if c_idx == 0 else 2*hidden_channels[c_idx]  # Skip connection from encoder
+                if mesh == "equirectangular":
+                    layer.append(CylinderPad(padding=1))
+                    layer.append(ResiduaBlock,
+                        in_channels=c_in_ if n_conv == 0 else c_out,
+                        out_channels=c_out)
+                    layer.append(self.attn)
+                    
+                # elif mesh == "healpix":
+                #     layer.append(HEALPixLayer(
+                #         layer=th.nn.Conv2d,
+                #         in_channels=c_in_ if n_conv == 0 else c_out,
+                #         out_channels=c_out,
+                #         kernel_size=3,
+                #         padding=1
+                #     ))
+
+                
+            # Apply upsampling if not in top-most layer
+            if c_idx < len(hidden_channels)-1:
+                layer.append(th.nn.ConvTranspose2d(
+                    in_channels=c_out,
+                    out_channels=hidden_channels[c_idx+1],
+                    kernel_size=4, # see pdf refiner paper
+                    stride=2
+                ))
+
+            self.layers.append(th.nn.Sequential(*layer))
+
+        self.layers = th.nn.ModuleList(self.layers)
+
+        
+        # Add linear output layer
+        self.output_layer = th.nn.Conv2d(
+            in_channels=c_out,
+            out_channels=out_channels,
+            kernel_size=3,
+            stride = 1
+        )
+
+        self.final_norm = th.nn.GroupNorm(8, c_out)
+
+    def forward(self, x: th.Tensor, skips: list) -> th.Tensor:
+        for l_idx, layer in enumerate(self.layers):
+            x = th.cat([skips[l_idx], x], dim=1) if l_idx > 0 else x
+            x = layer(x)
+        return self.output_layer(self.activation(self.final_norm(x)))
+    
+
+# class UpBlock(ConditionedBlock):
+#     """Up block This combines `ResidualBlock` and `AttentionBlock`.
+
+#     These are used in the second half of U-Net at each resolution.
+
+#     Args:
+#         in_channels (int): Number of input channels
+#         out_channels (int): Number of output channels
+#         cond_channels (int): Number of channels in the conditioning vector.
+#         has_attn (bool): Whether to use attention block
+#         activation (str): Activation function
+#         norm (bool): Whether to use normalization
+#         use_scale_shift_norm (bool): Whether to use scale and shift approach to conditoning (also termed as `AdaGN`).
+#         n_dims (int): Number of spatial dimensions. Defaults to 1.
+#     """
+
+#     def __init__(
+#         self,
+#         in_channels: int,
+#         out_channels: int,
+#         cond_channels: int,
+#         has_attn: bool = False,
+#         activation: str = "gelu",
+#         norm: bool = False,
+#         use_scale_shift_norm: bool = False,
+#         n_dims: int = 1,
+#     ):
+#         super().__init__()
+#         # The input has `in_channels + out_channels` because we concatenate the output of the same resolution
+#         # from the first half of the U-Net
+#         self.res = ResidualBlock(
+#             in_channels + out_channels,
+#             out_channels,
+#             cond_channels,
+#             activation=activation,
+#             norm=norm,
+#             use_scale_shift_norm=use_scale_shift_norm,
+#             n_dims=n_dims,
+#         )
+#         if has_attn:
+#             self.attn = AttentionBlock(out_channels)
+#         else:
+#             self.attn = nn.Identity()
+
+#     def forward(self, x: torch.Tensor, emb: torch.Tensor) -> torch.Tensor:
+#         x = self.res(x, emb)
+#         x = self.attn(x)
+#         return x
 
 
 if __name__ == "__main__":
